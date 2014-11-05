dc14b2c5fe57493f05a115b2b8ee63c85f35cdda
kevenking <kevenking@gmail.com>
2014-11-05 16:13:50 +0800

    test filter_tags function

--- utils/create_indexs.py
+++ utils/create_indexs.py
@@ -4,6 +4,26 @@ import pymongo
 import re
 from elasticsearch import Elasticsearch
 
+
+def filter_tags(htmlstr):  
+  #先过滤CDATA  
+  re_cdata=re.compile('//<!\[CDATA\[[^>]*//\]\]>',re.I) #匹配CDATA  
+  re_script=re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>',re.I)#Script  
+  re_style=re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>',re.I)#style  
+  re_br=re.compile('<br\s*?/?>')#处理换行  
+  re_h=re.compile('</?\w+[^>]*>')#HTML标签  
+  re_comment=re.compile('<!--[^>]*-->')#HTML注释  
+  s=re_cdata.sub('',htmlstr)#去掉CDATA  
+  s=re_script.sub('',s) #去掉SCRIPT  
+  s=re_style.sub('',s)#去掉style  
+  s=re_br.sub('\n',s)#将br转换为换行  
+  s=re_h.sub('',s) #去掉HTML 标签  
+  s=re_comment.sub('',s)#去掉HTML注释  
+  #去掉多余的空行  
+  blank_line=re.compile('\n+')  
+  s=blank_line.sub('\n',s)  
+  return s
+
 reComments = re.compile('<!--[^>]*-->')
 reHtml = re.compile('</?\w+[^>]*>')
 es = Elasticsearch()
@@ -101,7 +121,8 @@ listWebArticles = tWebArticles.find()
 for i in listWebArticles:
   if i['publishTime'] == '':
     continue
-  print reComments.sub('',reHtml.sub('',i['content']))
+  #print reComments.sub('',reHtml.sub('',i['content']))
+  print filter_tags(i['content'])
   print '='*30
   #es.index(index='web-articles',doc_type='article', body={'sitename':i['siteName'],'addtime':i['addTime'],'publishtime':i['publishTime'],'keywords':i['keyWords'],'url':i['url'],'title':i['title'],'content':reHtml.sub('',i['content'])})
 
@@ -114,3 +135,7 @@ listWebBlogs = tWebBlogs.find()
 for i in listWebBlogs:
   es.index(index='web-articles',doc_type='blog',timeout='2m', body={'sitename':i['siteName'],'addtime':i['addTime'],'url':i['url'],'title':i['title'],'content':reHtml.sub('',i['content']),'author':i['author']})
 '''
+
+
+
+
