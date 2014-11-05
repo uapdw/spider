37d4bda29945917deb601916eed32bbd584ff2c0
kevenking <kevenking@gmail.com>
2014-11-05 16:29:04 +0800

    test content html filter

--- utils/create_indexs.py
+++ utils/create_indexs.py
@@ -16,12 +16,12 @@ def filter_tags(htmlstr):
   s=re_cdata.sub('',htmlstr)#去掉CDATA  
   s=re_script.sub('',s) #去掉SCRIPT  
   s=re_style.sub('',s)#去掉style  
-  s=re_br.sub('\n',s)#将br转换为换行  
+  s=re_br.sub('',s)#将br转换为换行  
   s=re_h.sub('',s) #去掉HTML 标签  
   s=re_comment.sub('',s)#去掉HTML注释  
   #去掉多余的空行  
   blank_line=re.compile('\n+')  
-  s=blank_line.sub('\n',s)  
+  s=blank_line.sub('',s)  
   return s
 
 reComments = re.compile('<!--[^>]*-->')
@@ -112,29 +112,23 @@ es.indices.put_mapping(
 			}
 		)
 
-'''
 listBaiduArticles = tBaiduArticles.find()
 for i in listBaiduArticles:
-  es.index(index='web-articles',doc_type='baidu', body={'sitename':i['siteName'],'publishtime':i['publishTime'],'url':i['url'],'title':i['title'],'keywords':i['keyWords'],'content':reHtml.sub('',i['content']),'addtime':i['addTime']})
-'''
+  es.index(index='web-articles',doc_type='baidu', body={'sitename':i['siteName'],'publishtime':i['publishTime'],'url':i['url'],'title':i['title'],'keywords':i['keyWords'],'content':filter_tags(i['content']).strip(),'addtime':i['addTime']})
+
 listWebArticles = tWebArticles.find()
 for i in listWebArticles:
   if i['publishTime'] == '':
     continue
-  #print reComments.sub('',reHtml.sub('',i['content']))
-  print filter_tags(i['content'])
-  print '='*30
-  #es.index(index='web-articles',doc_type='article', body={'sitename':i['siteName'],'addtime':i['addTime'],'publishtime':i['publishTime'],'keywords':i['keyWords'],'url':i['url'],'title':i['title'],'content':reHtml.sub('',i['content'])})
+  es.index(index='web-articles',doc_type='article', body={'sitename':i['siteName'],'addtime':i['addTime'],'publishtime':i['publishTime'],'keywords':i['keyWords'],'url':i['url'],'title':i['title'],'content':filter_tags(i['content']).strip()})
 
-'''
 listInfReport = tInfReport.find()
 for i in listInfReport:
   es.index(index='web-articles',doc_type='report', body={'sitename':i['siteName'],'addtime':i['addTime'],'publishtime':i['publishTime'],'infsource':i['InfSource'],'url':i['url'],'title':i['title']})
 
 listWebBlogs = tWebBlogs.find()
 for i in listWebBlogs:
-  es.index(index='web-articles',doc_type='blog',timeout='2m', body={'sitename':i['siteName'],'addtime':i['addTime'],'url':i['url'],'title':i['title'],'content':reHtml.sub('',i['content']),'author':i['author']})
-'''
+  es.index(index='web-articles',doc_type='blog',timeout='2m', body={'sitename':i['siteName'],'addtime':i['addTime'],'url':i['url'],'title':i['title'],'content':filter_tags(i['content']).strip(),'author':i['author']})
 
 
 
