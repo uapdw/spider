3ea7a182ec340b3fdbde232330ef53b7a8f2cbb3
刘峰 <mainliufeng@gmail.com>
2015-12-07 14:26:49 +0800

    搜狐新闻

---
+++ spider/spiders/sohu_com/sohu_com_news.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class SohuNewsSpider(NewsSpider):
+    '''搜狐新闻爬虫'''
+
+    name = 'sohu_com_news'
+    allowed_domains = ['sohu.com']
+    start_urls = ['http://www.sohu.com/']
+
+    target_urls = [
+        '.*?sohu.com/\d{8}/n\d+.shtml'
+    ]
+
+    title_xpath = '//h1[@itemprop="headline"]'
+    content_xpath = '//div[@itemprop="articleBody"]'
+    author_xpath = '//*[@id="author_baidu"]'
+    author_re = u'.*?作者：\s*(\S+).*'
+    publish_time_xpath = '//*[@class="time"]'
+    publish_time_format = '%Y-%m-%d %H:%M:%S'
+    source_xpath = '//*[@id="source_baidu"]'
+    source_re = u'.*?来源：\s*(\S+).*'
+
+    source_domain = 'sohu.com'
+    source_name = '搜狐'
