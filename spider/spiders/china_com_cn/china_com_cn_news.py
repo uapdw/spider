d702bae091e42ff8f309ea3665d5f6774619da19
刘峰 <mainliufeng@gmail.com>
2015-12-08 16:36:37 +0800

    中国网新闻，网站不规范，很多页面xpath规则不一致

---
+++ spider/spiders/china_com_cn/china_com_cn_news.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class ChinaComCnNewsSpider(NewsSpider):
+    '''中国网新闻爬虫'''
+
+    name = 'china_com_cn_news'
+    allowed_domains = ['china.com.cn']
+    start_urls = ['http://www.china.com.cn/']
+
+    target_urls = [
+        'china\.com\.cn/\d{4}-\d{2}/\d{2}/content_\d+\.htm',
+        'china\.com\.cn/.*/\d{8}/\d+\.shtml'
+    ]
+
+    title_xpath = '//h1'
+    content_xpath = '//*[@id="artbody" or @id="fontzoom" or \
+                    @class="content" or @class="Content"]'
+    author_xpath = '//*[@id="author_baidu"]'
+    author_re = u'.*?作者：\s*(\S+).*'
+    publish_time_xpath = '//*[@id="pubtime_baidu"]'
+    publish_time_re = u'.*?(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}).*'
+    publish_time_format = '%Y-%m-%d %H:%M:%S'
+    source_xpath = '//*[@id="source_baidu"]'
+    source_re = u'.*?来源：\s*(\S+).*'
+
+    source_domain = 'china.com.cn'
+    source_name = '中国网'
