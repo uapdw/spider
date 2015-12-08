b5e030ab41aeaa2b1a16b651f46df8f522836ed8
刘峰 <mainliufeng@gmail.com>
2015-12-08 16:00:57 +0800

    天极网新闻

---
+++ spider/spiders/yesky_com/yesky_com_news.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class YeskyComNewsSpider(NewsSpider):
+    '''天极网新闻爬虫'''
+
+    name = 'yesky_com_news'
+    allowed_domains = ['yesky.com']
+    start_urls = ['http://www.yesky.com/']
+
+    target_urls = [
+        'yesky.com/\d+/\d+.shtml'
+    ]
+
+    title_xpath = '//*[@class="title"]'
+    content_xpath = '//*[@class="article"]'
+    author_xpath = '//*[@class="editor"]'
+    author_re = u'.*?作者：\s*(\S+).*'
+    publish_time_xpath = '//*[@class="detail"]/span[2]'
+    publish_time_format = '%Y-%m-%d %H:%M'
+    source_xpath = '//*[@class="detail"]/span[1]'
+
+    source_domain = 'yesky.com'
+    source_name = '天极网'
