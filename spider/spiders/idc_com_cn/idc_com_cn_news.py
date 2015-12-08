006b0d29a3231c60039ce4527315d5ffcae2bbf1
刘峰 <mainliufeng@gmail.com>
2015-12-08 19:26:56 +0800

    IDC新闻

---
+++ spider/spiders/idc_com_cn/idc_com_cn_news.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class IdcNewsSpider(NewsSpider):
+    '''idc新闻爬虫'''
+
+    name = 'idc_com_cn_news'
+    allowed_domains = ['idc.com.cn']
+    start_urls = ['http://www.idc.com.cn/']
+
+    target_urls = [
+        'idc\.com\.cn/about/press\.jsp\?id=\S+'
+    ]
+
+    title_xpath = '//*[@class="bodybkbd"]'
+    content_xpath = '//*[@class="bodybk"]'
+    publish_time_xpath = '//*[@class="bodybk"]'
+    publish_time_re = u'.*日期：\s*(\d{4}-\d{2}-\d{2}).*'
+    publish_time_format = '%Y-%m-%d'
+    source_xpath = '//*[@class="bodybk"]'
+    source_re = u'.*信息来源：\s*(\S+).*'
+
+    source_domain = 'idc.com.cn'
+    source_name = 'IDC'
