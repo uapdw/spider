656a3d626ed7ab9c986e6ab81bde46f0e198db4b
刘峰 <mainliufeng@gmail.com>
2015-12-09 09:18:40 +0800

    财经网新闻

---
+++ spider/spiders/caijing_com_cn/caijing_com_cn_news.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class CaijingNewsSpider(NewsSpider):
+    '''财经网新闻爬虫'''
+
+    name = 'caijing_com_cn_news'
+    allowed_domains = ['caijing.com.cn']
+    start_urls = ['http://www.caijing.com.cn/']
+
+    target_urls = [
+        'caijing\.com\.cn/\d{8}/\d+\.shtml'
+    ]
+
+    title_xpath = '//*[@id="cont_title"]'
+    content_xpath = '//*[@id="the_content"]'
+    author_xpath = '//*[@id="editor_baidu"]'
+    author_re = u'.*?编辑：\s*(\S+)\).*'
+    publish_time_xpath = '//*[@id="pubtime_baidu"]'
+    publish_time_format = '%Y-%m-%d %H:%M:%S'
+    source_xpath = '//*[@id="source_baidu"]'
+
+    source_domain = 'caijing.com.cn'
+    source_name = u'财经网'
