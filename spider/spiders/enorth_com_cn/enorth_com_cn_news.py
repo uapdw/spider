5bb575ecf923aa670bd4a75dea6655456654e758
zhangyth <zhangyth@yonyou.com>
2015-12-14 16:08:59 +0800

    北方网新闻
    Change-Id: I1e6740dbe877fad57187541224c4dcc7a6cd889e

---
+++ spider/spiders/enorth_com_cn/enorth_com_cn_news.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class ENorthNewsSpider(NewsSpider):
+    '''北方网新闻爬虫'''
+
+    name = 'enorth_com_cn_news'
+    allowed_domains = ['enorth.com.cn']
+    start_urls = ['http://www.enorth.com.cn/']
+
+    target_urls = [
+        '\w+\.enorth.com.cn/\w+/\d{4}/\d{2}/\d{2}/|w+.shtml'
+    ]
+
+    title_xpath = '//*[@class="title heiti zi24 yanse1"]'
+    content_xpath = '//*[@class="zi14 hanggao24"]'
+    author_xpath = '//*[@id="author_baidu"]'
+    author_re = u'.*?作者：(\S+).*'
+    publish_time_xpath = '//*[@id="pubtime_baidu"]'
+    publish_time_re = u'.*?(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2}).*'
+    publish_time_format = '%Y-%m-%d%H:%M:%S'
+    source_xpath = '//*[@id="source_baidu"]'
+    source_re = u'.*?来源：\s*(\S+).*'
+
+    source_domain = 'enorth.com.cn'
+    source_name = '北方网'
