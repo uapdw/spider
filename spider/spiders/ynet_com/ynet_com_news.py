4bd6d2f8b860a3628ef48d71d78d6ad0284d22ec
刘峰 <mainliufeng@gmail.com>
2015-12-14 14:47:25 +0800

    北青网新闻

---
+++ spider/spiders/ynet_com/ynet_com_news.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class YnetNewsSpider(NewsSpider):
+
+    u"""北青网新闻爬虫"""
+
+    name = 'ynet_com_news'
+    allowed_domains = ['ynet.com']
+    start_urls = ['http://www.ynet.com/']
+
+    target_urls = [
+        'news\.ynet\.com/[\d\.]+/\d{4}/\d{2}/\d+\.html'
+    ]
+
+    title_xpath = '//*[@class=" BSHARE_POP"]'
+    content_xpath = '//*[@id="pzoom"]'
+    publish_time_xpath = '//*[@id="pubtime_baidu"]'
+    publish_time_format = '%Y-%m-%d %H:%M:%S'
+    source_xpath = '//*[@id="source_baidu"]'
+    source_re = u'.*?来源：\s*(\S+).*'
+
+    source_domain = 'ynet.com'
+    source_name = u'北青网'
