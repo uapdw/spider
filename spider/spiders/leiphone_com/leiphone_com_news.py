fc84047478773d51514fd777dee866a8f31a93a1
刘峰 <mainliufeng@gmail.com>
2015-12-15 09:12:38 +0800

    雷锋网新闻

---
+++ spider/spiders/leiphone_com/leiphone_com_news.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from spider.spiders import NewsSpider
+
+
+class LeiphoneNewsSpider(NewsSpider):
+
+    u"""雷锋网新闻爬虫"""
+
+    name = 'leiphone_com_news'
+    allowed_domains = ['leiphone.com']
+    start_urls = ['http://www.leiphone.com/']
+
+    target_urls = [
+        'leiphone\.com/news/\d{6}/\S+\.html'
+    ]
+
+    title_xpath = '//*[@class="pageTop"]/h1'
+    content_xpath = '//*[contains(@class, "pageCont")]'
+    author_xpath = '//*[@class="pi-author"]'
+    author_re = u'.*\d{4}-\d{2}-\d{2}\s*\d{2}:\d{2}\s*\S+\s*(\S+).*'
+    publish_time_xpath = '//*[@class="pi-author"]'
+    publish_time_re = u'.*(\d{4}-\d{2}-\d{2} \d{2}:\d{2}).*'
+    publish_time_format = '%Y-%m-%d %H:%M'
+    source_xpath = '//*[@class="pi-author"]'
+    source_re = u'.*\d{4}-\d{2}-\d{2}\s*\d{2}:\d{2}\s*(\S+)\s*\S+.*'
+
+    source_domain = 'leiphone.com'
+    source_name = u'雷锋网'
