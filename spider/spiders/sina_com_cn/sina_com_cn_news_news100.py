66ddd761b6327162b1431e22f4c57e566f90d8e4
biancm <biancm@yonyou.com>
2016-02-05 17:01:23 +0800

    改名

--- spider/spiders/sina_com_cn/sina_com_cn_news_news100.py
+++ spider/spiders/sina_com_cn/sina_com_cn_news_news100.py
@@ -15,7 +15,7 @@ class SinaComCnNews100NewsSpider(Spider):
     name = 'sina_com_cn_news_100news'
     allowed_domain = ['auto.sina.com.cn']
     api_url_pre = 'http://news.auto.sina.com.cn/m/label/get_label_info.php?label=%E8%AF%84%E8%AE%BA%2C%E4%BA%A7%E4%B8%9A%2C%E5%8D%A1%E8%BD%A6%2C%E5%AE%A2%E8%BD%A6%2C%E4%BC%81%E4%B8%9A%2C%E8%B4%A2%E7%BB%8F%2C%E5%8F%AC%E5%9B%9E%2C%E8%90%A5%E9%94%80%2C%E8%AE%BF%E8%B0%88'
-    api_url_pattern = '&length=%s&page=%s'
+    api_url_pattern = '&length=%s&page=%s' 
 
     page_size = 100
 
