82e70b1b83e4ccb6961b783a3e9457d680be7c4d
刘峰 <mainliufeng@gmail.com>
2016-02-18 16:41:15 +0800

    LoaderMappingSpider and Example

--- spider/spiders/100ec_cn/100ec_cn_news.py
+++ spider/spiders/100ec_cn/100ec_cn_news.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from spider.spiders import NewsSpider
+from spider.spiders import LoaderMappingSpider
+from spider.loader.loaders import N100ecNewsLoader
 
 
-class N100ecNewsSpider(NewsSpider):
+class N100ecNewsSpider(LoaderMappingSpider):
 
     u"""中国电子商务研究中心新闻爬虫"""
 
@@ -11,18 +12,6 @@ class N100ecNewsSpider(NewsSpider):
     allowed_domains = ['100ec.cn']
     start_urls = ['http://www.100ec.cn']
 
-    target_urls = [
-        '100ec\.cn/detail--\d+\.html'
-    ]
-
-    title_xpath = '//*[@class="newsview"]/h2'
-    content_xpath = '//*[@class="nr"]'
-    publish_time_xpath = '//*[@class="public f_hong"]'
-    publish_time_re = u'.*(\d{4})年(\d{2})月(\d{2})日(\d{2}:\d{2}).*'
-    publish_time_re_join = '-'
-    publish_time_format = '%Y-%m-%d-%H:%M'
-    source_xpath = '//*[@class="public f_hong"]'
-    source_re = u'.*?(\S+)\s*\d{4}年\d{2}月\d{2}日\d{2}:\d{2}.*'
-
-    site_domain = '100ec.com'
-    site_name = u'中国电子商务研究中心'
+    mapping = {
+        '100ec\.cn/detail--\d+\.html': N100ecNewsLoader
+    }
--- spider/spiders/__init__.py
+++ spider/spiders/__init__.py
@@ -12,6 +12,37 @@ from spider.loader.processors import (text, DateProcessor, PipelineProcessor,
                                       RegexProcessor, SafeHtml)
 
 
+class LoaderMappingSpider(CrawlSpider):
+
+    mapping = None
+
+    def __init__(self):
+        if not getattr(self, 'mapping', None):
+            raise ValueError(
+                "%s must have a mapping" % type(self).__name__
+            )
+        self.__generate_rules()
+        super(LoaderMappingSpider, self).__init__()
+
+    def __generate_rules(self):
+        '''生成scrapy.contrib.spiders.CrawlSpider中的rules
+        '''
+        rule_list = []
+        for url, loader in self.mapping.iteritems():
+            rule = Rule(
+                LinkExtractor(allow=(url),
+                              allow_domains=self.allowed_domains),
+                callback=lambda response: loader().load(response),
+                follow=False)
+            rule_list.append(rule)
+        rule_list.append(
+            Rule(
+                LinkExtractor(allow=('.*'), allow_domains=self.allowed_domains)
+            )
+        )
+        self.rules = tuple(rule_list)
+
+
 class TargetUrlCallbackMappingSpider(CrawlSpider):
     '''目标链接回调映射爬虫
     url_callback_mapping中设置，目标链接正则:这类链接的callback函数
