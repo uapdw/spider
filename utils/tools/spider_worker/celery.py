e7da6091b19632ffbe3bdd4ee3414128c9a28c32
刘峰 <mainliufeng@gmail.com>
2016-04-14 20:04:59 +0800

    redis ip

--- utils/tools/spider_worker/celery.py
+++ utils/tools/spider_worker/celery.py
@@ -25,10 +25,10 @@ app = Celery('spider_worker')
 # app.config_from_object('config')
 
 app.conf.update(
-    #BROKER_URL='redis://172.20.14.29:6379/0',
-    #CELERY_RESULT_BACKEND='redis://172.20.14.29:6379/1',
-    BROKER_URL='redis://127.0.0.1:6379/0',
-    CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/1',
+    BROKER_URL='redis://172.20.14.29:6379/0',
+    CELERY_RESULT_BACKEND='redis://172.20.14.29:6379/1',
+    #BROKER_URL='redis://127.0.0.1:6379/0',
+    #CELERY_RESULT_BACKEND='redis://127.0.0.1:6379/1',
     CELERY_TASK_SERIALIZER='json',
     CELERY_ACCEPT_CONTENT=['json'],
     CELERY_RESULT_SERIALIZER='json',
