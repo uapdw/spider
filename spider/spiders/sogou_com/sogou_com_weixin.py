138f260a91583a033d092fdc5cafbdcc41da09f1
kevenking <kevenking@gmail.com>
2016-05-07 11:39:55 +0800

    将微信爬虫修改为采集搜狗上展示的全部微信内容

--- spider/spiders/sogou_com/sogou_com_weixin.py
+++ spider/spiders/sogou_com/sogou_com_weixin.py
@@ -35,16 +35,42 @@ class SogouWeixinSpider(CrawlSpider):
             LinkExtractor(
                 allow=('.*'),
                 allow_domains=allowed_domains,
+                restrict_xpaths=('//div[@id="page-content"]')
+            ),
+            callback='parse_weixin'
+        ),
+        Rule(
+            LinkExtractor(
+                allow=('.*'),
+                allow_domains=allowed_domains,
                 restrict_xpaths=('//*[@id="sogou_next"]')
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                allow=('.*'),
+                allow_domains=allowed_domains,
+                restrict_xpaths=('//div[@class="wx-news-info2"]')
+            )
+        ),
+        Rule(
+            LinkExtractor(
+                allow=('mp\.weixin\.qq\.com\/profile.*'),
+                allow_domains=allowed_domains,
+                restrict_xpaths=('//div[@class="weui_msg_card_list"]')
             )
         ),
     )
 
     def __init__(self, keyword=None, *args, **kwargs):
         super(SogouWeixinSpider, self).__init__(*args, **kwargs)
-        self.start_urls = [
-            'http://weixin.sogou.com/weixin?type=2&query=%s' % keyword
-        ]
+        for i in range(0, 20):
+            self.start_urls.append('http://weixin.sogou.com/pcindex/pc/pc_%s/pc_%s.html' % (i, i))
+            for page in range(1,16):
+                self.start_urls.append('http://weixin.sogou.com/pcindex/pc/pc_%s/%s.html' % (i, page))
+        #self.start_urls = [
+        #    #'http://weixin.sogou.com/weixin?type=2&query=%s' % keyword
+        #]
 
     def parse_weixin(self, response):
 
