d19738c536f6bb8d4f6f22ccdc24b5ec42fd6d55
刘峰 <mainliufeng@gmail.com>
2016-05-10 09:37:00 +0800

    利润表、资产负债表Item

--- spider/items.py
+++ spider/items.py
@@ -148,7 +148,7 @@ class ListedCorpInfo(Base):
 
 
 class ListedCorpInfoItem(SqlalchemyItem):
-    required_fields = ['stock_cd', 'year', 'period']
+    required_fields = ['stock_cd', 'year', 'period', 'data_sour']
     model = ListedCorpInfo
 
     data_sour = Field()
@@ -258,6 +258,83 @@ class AsstLiabTable(Base):
     modifytime = Column(String(19))
 
 
+class AsstLiabTableItem(SqlalchemyItem):
+    required_fields = ['stock_cd', 'year', 'period', 'data_sour']
+    model = AsstLiabTable
+
+    year = Field()
+    period = Field()
+    stock_cd = Field()
+    data_sour = Field()
+    curr_fund = Field()
+    notes_recev = Field()
+    txn_fin_ast = Field()
+    reces = Field()
+    prepay = Field()
+    oth_recev = Field()
+    recev_afflt_account = Field()
+    recev_intr = Field()
+    recev_dividn = Field()
+    inventy = Field()
+    consum_bio_ast = Field()
+    oyear_not_current_ast = Field()
+    other_current_ast = Field()
+    current_ast_sum = Field()
+    saleable_fin_ast = Field()
+    hold_investm_due = Field()
+    lterm_reces = Field()
+    lterm_equity_investm = Field()
+    real_estate_investm = Field()
+    fixed_ast = Field()
+    under_constr_proj = Field()
+    proj_goods = Field()
+    fixed_ast_clean = Field()
+    prod_bio_ast = Field()
+    oil_ast = Field()
+    intang_ast = Field()
+    develop_costs = Field()
+    goodwill = Field()
+    deferred_ast = Field()
+    deferred_tax_ast = Field()
+    oth_non_current_ast = Field()
+    non_current_ast_sum = Field()
+    ast_sum = Field()
+    sterm_liab = Field()
+    txn_fin_liab = Field()
+    notes_payable = Field()
+    accounts_payable = Field()
+    adv_account = Field()
+    payroll_payable = Field()
+    tax_payable = Field()
+    intr_payable = Field()
+    dividn_payable = Field()
+    oth_payable = Field()
+    due_related_corp = Field()
+    oyear_not_current_liab = Field()
+    oth_current_liab = Field()
+    current_liab_sum = Field()
+    lterm_loan = Field()
+    bonds_payable = Field()
+    term_payable = Field()
+    spec_payable = Field()
+    estim_liab = Field()
+    deferr_inc_tax_liab = Field()
+    oth_not_current_liab = Field()
+    not_current_liab_sum = Field()
+    liab_sum = Field()
+    real_reces_cap = Field()
+    cap_reserve = Field()
+    earned_surplus = Field()
+    treas_stock = Field()
+    undistr_profit = Field()
+    minority_equity = Field()
+    fcurr_trans_spreads = Field()
+    abnorm_run_proj_inc_adjust = Field()
+    owner_intr_sum = Field()
+    liab_owner_sum = Field()
+    modifytime = Field()
+
+
 class ProfitTable(Base):
     __tablename__ = 'profit_table'
 
@@ -289,6 +366,40 @@ class ProfitTable(Base):
     nprf_attrib_parent_corp = Column(DECIMAL(24, 2))
     less_intr_income = Column(DECIMAL(24, 2))
     modifytime = Column(DECIMAL(24, 2))
+
+
+class ProfitTableItem(SqlalchemyItem):
+    required_fields = ['stock_cd', 'year', 'period', 'data_sour']
+    model = ProfitTable
+
+    year = Field()
+    period = Field()
+    stock_cd = Field()
+    data_sour = Field()
+    biz_income = Field()
+    biz_cost = Field()
+    sell_cost = Field()
+    manage_cost = Field()
+    explor_cost = Field()
+    fin_cost = Field()
+    ast_devalu_loss = Field()
+    fair_value_chng_net_inc = Field()
+    inv_prft = Field()
+    invest_assoc_joint_comp = Field()
+    operat_prft_oth_subj = Field()
+    run_prft = Field()
+    subs_reven = Field()
+    nonbiz_incom = Field()
+    nonbiz_cost = Field()
+    ncurrt_ast_dispos_nloss = Field()
+    oth_subj_affect_total_prft = Field()
+    profit_tamt = Field()
+    income_tax = Field()
+    oth_subj_affect_net_prft = Field()
+    net_profit = Field()
+    nprf_attrib_parent_corp = Field()
+    less_intr_income = Field()
+    modifytime = Field()
 
 
 class CashFlowTable(Base):
@@ -330,6 +441,48 @@ class CashFlowTable(Base):
     cash_outflow_fina_activ = Column(DECIMAL(24, 2))
     ncash_flow_make_fina_activ = Column(DECIMAL(24, 2))
     modifytime = Column(DECIMAL(24, 2))
+
+
+class CashFlowTableItem(SqlalchemyItem):
+    required_fields = ['stock_cd', 'year', 'period', 'data_sour']
+    model = ProfitTable
+
+    year = Field()
+    period = Field()
+    stock_cd = Field()
+    data_sour = Field()
+    cash_recev_sell_goods = Field()
+    refund_taxes = Field()
+    cash_recev_oth_run_biz = Field()
+    operat_activ_cash_inflows = Field()
+    cash_paid_buy_goods = Field()
+    tax_paym = Field()
+    cash_paid_staff = Field()
+    cash_paid_oth_run_biz = Field()
+    operat_activ_cash_outflow = Field()
+    operat_activ_cash_flow_net = Field()
+    cash_recev_invests = Field()
+    cash_recev_invest_intr = Field()
+    net_cash_recev_disp_fix_ast = Field()
+    net_cash_recev_oth_biz = Field()
+    recev_oth_invest_activ_cash = Field()
+    cash_inflow_invest_activ = Field()
+    cash_paid_constr_fixed_ast = Field()
+    inv_payment = Field()
+    net_cash_acqu_oth_biz_units = Field()
+    pay_oth_invest_activ_cash = Field()
+    cash_outflow_invest_activ = Field()
+    net_cashflow_make_invest_activ = Field()
+    cash_recev_invest = Field()
+    cash_recev_debts = Field()
+    oth_fin_activ_recv_cash = Field()
+    fina_activ_cash_inflow = Field()
+    debt_payment = Field()
+    pay_intr_cash = Field()
+    cash_payment_rela_fina_activ = Field()
+    cash_outflow_fina_activ = Field()
+    ncash_flow_make_fina_activ = Field()
+    modifytime = Field()
 
 
 class StockReport(Base):
