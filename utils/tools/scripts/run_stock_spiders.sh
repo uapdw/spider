ba4a894eed4b7221bfed01788fec207dac06c892
刘峰 <mainliufeng@gmail.com>
2016-05-13 15:16:06 +0800

    运行上市公司爬虫脚本（根据已有公司列表、时间数据抓取公司信息和三大报表）

---
+++ utils/tools/scripts/run_stock_spiders.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /data0/sourcecode/spider/current/;
+source ~/.bash_profile;
+workon spider;
+for n in "cninfo_com_cn_listed_corp_info" "cninfo_com_cn_balance" "cninfo_com_cn_profit" "cninfo_com_cn_cashflow" "cninfo_com_cn_pdf"
+do
+    nohup scrapy crawl $n > /data0/log/$n.log 2>&1 &
+done
