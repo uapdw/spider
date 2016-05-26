515a7936252d92cea1d3134eab1151ee57dc013c
root <root@test-spider-02.(none)>
2016-05-26 10:36:48 +0800

    spiders

---
+++ spider/spiders/chinatax_gov_cn/chinatax_gov_cn_news.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from scrapy.spider import Spider
+from scrapy.linkextractors import LinkExtractor
+
+from spider.spiders import LoaderMappingSpider
+from spider.loader.loaders.chinatax_gov_cn.chinatax_gov_cn_news import(
+    ChinataxGovCnNewsLoader
+)
+
+
+class ChinataxGovCnNewsSpider(LoaderMappingSpider):
+
+    u"""国家税务总局新闻爬虫"""
+
+    name = 'chinatax_gov_cn_news'
+    allowed_domains = [
+        'chinatax.gov.cn'
+    ]
+    start_urls = ['http://www.chinatax.gov.cn/index.html']
+
+    mapping = {
+        'chinatax\.gov\.cn/n\d+/n\d+/n\d+/c\d+/content.html':
+        ChinataxGovCnNewsLoader
+    }
