c1b581eae07b5299bdf25628b7f3b93b3603c33b
刘峰 <mainliufeng@gmail.com>
2016-11-30 16:01:06 +0800

    update scripts

--- utils/tools/scripts/scrapy_run_stock_spiders.sh
+++ utils/tools/scripts/scrapy_run_stock_spiders.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cd /data0/sourcecode/spider/current/;
+source ~/.bash_profile;
+workon spider;
 for n in "cninfo_com_cn_listed_corp_info" "cninfo_com_cn_balance" "cninfo_com_cn_profit" "cninfo_com_cn_cashflow"
 do
-    nohup /root/.virtualenvs/spider/bin/scrapy crawl $n > /data0/log/$n.log 2>&1 &
+    nohup scrapy crawl $n > /data0/log/$n.log 2>&1 &
 done
--- utils/tools/scripts/scrapy_stock_company.sh
+++ utils/tools/scripts/scrapy_stock_company.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+cd /data0/sourcecode/spider/current/;
+source ~/.bash_profile;
+workon spider;
 for n in "szse_cn_curr_listed_corp" "sse_com_cn_sh_listed_corp_info" "cninfo_com_cn_curr_listed_corp"
 do
-    /root/.virtualenvs/spider/bin/scrapy crawl $n > /data0/log/$n.log 2>&1
+    scrapy crawl $n > /data0/log/$n.log 2>&1
 done
